3042e488954c7ec278f194b910fd0d9313532359
ycshope <442570684@qq.com>
2020-09-25 14:28:56 -0400

    2020.09.26

--- tuple.py
+++ tuple.py
@@ -8,7 +8,6 @@ def GetValue():
     
 #元组和list的转化
 def TypeExchange(t):
-    return(list(t)) #返回结果是一个list
     t_type = str(type(t))
     #if type(t) == "<class 'list'>":    这样写存在问题,因为type不是一个类型和"<class 'list'>"不是一个类型
     if t_type == "<class 'list'>":
@@ -21,6 +20,18 @@ def TypeExchange(t):
     else:
         print("3")
         pass
+
+'''
+def TypeExchange2(t):
+    print(type(t))              结果是<class 'tuple'>
+    print(type(tuple))          结果是<class 'type'>
+    if type(t) == type(tuple):      原因是类型不同
+        return list(t)
+    elif type(t) == type(list):
+        return tuple(t)
+    else:
+        pass
+'''
     
 if __name__ == "__main__":
     #元组支持多赋值
@@ -32,5 +43,7 @@ if __name__ == "__main__":
     print("after type change:")
     My_list = TypeExchange(My_tuple)
     print(My_list)
-    print(type(list))
+    print("*"*20)
+    print(TypeExchange2(My_list))
+    #print(TypeExchange2(My_tuple))
     
